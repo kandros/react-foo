9312baeb910e3cac8565f327e66a49da5d4ef079
jaga <kandros5591@gmail.com>
2017-01-11 22:27:46 +0100

    typecheck renderBlueprint

--- tasks/renderBlueprint.js
+++ tasks/renderBlueprint.js
@@ -5,12 +5,12 @@ const ejs = require('ejs')
 
 const capitalize = (n) => n.charAt(0).toUpperCase() + n.slice(1)
 
-module.exports = function renderBlueprint(bluePrintPath, entityName) {
+module.exports = function renderBlueprint(blueprintPath: string, entityName: string): string {
     let template
     try {
-        template = fs.readFileSync(`${process.cwd()}/${bluePrintPath}`, 'utf8')
+        template = fs.readFileSync(`${process.cwd()}/${blueprintPath}`, 'utf8')
     } catch(e) {
-        console.log(`Blueprint file doesn't exist in ${bluePrintPath}.`)
+        console.log(`Blueprint file doesn't exist in ${blueprintPath}.`)
         process.exit(1)
     }
 
