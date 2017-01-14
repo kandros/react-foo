1ab39c61efad3127fce1a91325df048b2986b4d9
jaga <kandros5591@gmail.com>
2017-01-14 10:39:03 +0100

    typecheck config file

--- react-foo.js
+++ react-foo.js
@@ -1,38 +1,62 @@
-module.exports = {
+// @flow
+import type {Config} from './types'
+
+const config: Config = {
     blueprints: [
         {
-            "name": "redux",
-            "description": "Generates a dumb react component in shared components",
-            "usage": "dumb-s [component-name]",
-            "files": [
+            'name': 'redux-form-v5',
+            'description': 'Generates a dumb react component in shared components',
+            'usage': 'dumb-s [component-name]',
+            'files': [
                 {
-                    "blueprint-path": "blueprints/redux/store.js",
-                    "target-path": "redux/store.js"
+                    'blueprint-path': 'blueprints/redux/store.js',
+                    'target-path': 'redux/store.js'
                 },
                 {
-                    "blueprint-path": "blueprints/redux/reducers/root-reducer.js",
-                    "target-path": "redux/reducers/root-reducer.js"
+                    'blueprint-path': 'blueprints/redux/reducers/root-reducer.js',
+                    'target-path': 'redux/reducers/root-reducer.js'
                 },
                 {
-                    "blueprint-path": "blueprints/redux/reducers/app-reducer.js",
-                    "target-path": "redux/reducers/app-reducer.js"
-                },
+                    'blueprint-path': 'blueprints/redux/reducers/app-reducer.js',
+                    'target-path': 'redux/reducers/app-reducer.js'
+                }
             ]
         },
         {
-            "name": "component",
-            "description": "Generates a dumb react component in shared components",
-            "usage": "dumb-s [component-name]",
-            "files": [
+            'name': 'redux',
+            'description': 'Generates a dumb react component in shared components',
+            'usage': 'dumb-s [component-name]',
+            'files': [
                 {
-                    "blueprint-path": "blueprints/component/container.ejs",
-                    "target-path": "components/<%= helpers.capitalize(name) %>/<%= helpers.capitalize(name) %>Container.js"
+                    'blueprint-path': 'blueprints/redux/store.js',
+                    'target-path': 'redux/store.js'
                 },
                 {
-                    "blueprint-path": "blueprints/component/presentational.ejs",
-                    "target-path": "components/<%= helpers.capitalize(name) %>/<%= helpers.capitalize(name) %>Dumb.js"
+                    'blueprint-path': 'blueprints/redux/reducers/root-reducer.js',
+                    'target-path': 'redux/reducers/root-reducer.js'
+                },
+                {
+                    'blueprint-path': 'blueprints/redux/reducers/app-reducer.js',
+                    'target-path': 'redux/reducers/app-reducer.js'
+                }
+            ]
+        },
+        {
+            'name': 'component',
+            'description': 'Generates a dumb react component in shared components',
+            'usage': 'dumb-s [component-name]',
+            'files': [
+                {
+                    'blueprint-path': 'blueprints/component/container.ejs',
+                    'target-path': 'components/<%= helpers.capitalize(name) %>/<%= helpers.capitalize(name) %>Container.js'
+                },
+                {
+                    'blueprint-path': 'blueprints/component/presentational.ejs',
+                    'target-path': 'components/<%= helpers.capitalize(name) %>/<%= helpers.capitalize(name) %>Presentational.js'
                 }
             ]
         }
     ]
 }
+
+module.exports = config
--- tasks/renderBlueprint.js
+++ tasks/renderBlueprint.js
@@ -1,3 +1,4 @@
+// @flow
 'use strict'
 
 const fs = require('fs')
